6346358aef736c91ace0b68b7bc732d5add05dcc
sarahaldrees <aldreessarah@gmail.com>
2022-04-19 10:54:19 +0300

    commit

--- app/src/main/java/com/example/sarahaldreesmidterm2/MainActivity.java
+++ app/src/main/java/com/example/sarahaldreesmidterm2/MainActivity.java
@@ -1,45 +1,61 @@
 package com.example.sarahaldreesmidterm2;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.ActionMenuView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView reservation;
+    String url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=8882c2421272cc9a4afc3d2938c9fb09&units=metric";
+    ImageView weatherBackground;
+    TextView temperature, humidity,reservation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        reservation=(TextView) findViewById(R.id.txtReservation);
-        Button button=(Button) findViewById(R.id.bttnDate);
-        final Button act2 = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        Button buttonDate=(Button) findViewById(R.id.btnDate);
+        reservation =(TextView) findViewById(R.id.datepicked);
+        temperature = findViewById(R.id.temperature);
+        humidity = findViewById(R.id.humidity);
+
+        buttonDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new DatePickerDialog(MainActivity.this,d,c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
-        act2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startActivity(new Intent(MainActivity.this,MainActivity2.class));}
-        });
+        weather(url);
     }
 
     Calendar c=Calendar.getInstance();
@@ -50,7 +66,44 @@ public class MainActivity extends AppCompatActivity {
             c.set(Calendar.YEAR, year);
             c.set(Calendar.MONTH, month);
             c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            reservation.setText("You have choosen "+fmtDate.format(c.getTime()));
+            reservation.setText("Your reservation is set for "+fmtDate.format(c.getTime()));
         }};
 
+    public void weather(String url){
+        @SuppressLint("SetTextI18n") JsonObjectRequest jsonObj = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+            Log.d("Sarah", "Response Received");
+            Log.d("Sarah", response.toString());
+            try {
+                JSONObject jsonMain = response.getJSONObject("main");
+                JSONObject jsonSys = response.getJSONObject("sys");
+
+                double temp = jsonMain.getDouble("temp");
+                Log.d("Sarah","temp=" + temp);
+                temperature.setText(String.valueOf(temp)+"°C");
+
+                double feels = jsonMain.getDouble("feels_like");
+
+                double humid = jsonMain.getDouble("humidity");
+                Log.d("Sarah","humidity=" + feels);
+                humidity.setText("Humidity: "+String.valueOf(humid));
+
+
+                JSONArray jsonArray = response.getJSONArray("weather");
+                for (int i=0; i<jsonArray.length();i++){
+                    Log.d("Sarah-array",jsonArray.getString(i));
+                    JSONObject oneObject = jsonArray.getJSONObject(i);
+                    String weather =
+                            oneObject.getString("main");
+                    Log.d("Sarah-w",weather);
+
+                }
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+                Log.e("Receive Error", e.toString());
+            }
+        }, error -> Log.d("Sarah", "Error Retrieving URL"));
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObj);
+    }
 }
